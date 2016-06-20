0b20a14216e6571d17926ac4a0fa8cfa721e536e
jamesyburr <vint.james@gmail.com>
2016-06-20 15:23:34 -0400

    added login and add message route

--- src/main/java/com/james/MicroBlogController.java
+++ src/main/java/com/james/MicroBlogController.java
@@ -15,12 +15,24 @@ import java.util.ArrayList;
 @Controller
 public class MicroBlogController {
 
-    ArrayList<Message> messages;
+    ArrayList<Message> messages = new ArrayList<>();
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home(Model model, HttpSession session, String username) {
-    String user = (String) session.getAttribute("username");
-    model.addAttribute("username", user);
-        return "";
+    public String home(Model model, HttpSession session) {
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("username", username);
+        return "home";
+    }
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(String username, HttpSession session) {
+        session.setAttribute("username", username);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/addmessage", method = RequestMethod.POST)
+    public String addMessage(String text, HttpSession session) {
+        Message message = new Message((messages.size()), text);
+        messages.add(message);
+        return "redirect:/";
     }
 }
